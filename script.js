6c5ef8eb3ab0378b39630ea515ae7cc344309513
CaptainSapphire <122335357+CaptainSapphire@users.noreply.github.com>
2024-05-21 11:09:18 -0700

    1 file modified

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log('Hello!');
+
+
+
+// User gets to the bottom
+window.onscroll = () => {
+  if((window.innerheight + window.pageYOffset) >= document.body.offsetHeight)
+    console.log("You are at the end of the page.")
+
+
+}
